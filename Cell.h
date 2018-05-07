eeca0ac3855f2daecb982a958efad6e070799b9e
neorus <neorus666@gmail.com>
2018-05-07 18:14:20 +0000

    ..

---
+++ Cell.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Cell{
+    private:
+       char pox;
+
+    public:
+        Cell();
+        Cell(const char &);
+        Cell& operator = (const char &);
+        char getpox();
+        operator char();
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Stream<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+        friend ostream& operator << (ostream &, Cell &);
+
+        ~Cell();
+};
+
+struct IllegalCharException : public exception {
+    char a;
+    IllegalCharException(char x){
+        a = x;
+    }
+    const char theChar() const throw () {
+        return a;
+   }
+};
