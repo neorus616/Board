ed1eeb6ba9b2bab4970139bc51bbdb43cb506e1c
neorus616 <33427780+neorus616@users.noreply.github.com>
2018-05-22 18:35:27 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,15 +4,6 @@ using namespace std;
 
 Player::Player(){}
 
-
-const string Player::name() const{
-     return "lolik"; 
-     }
-
-const Coordinate Player::play(const Board& board){
-    
-}
-
 void Player::setChar(char myC){
     myChar = myC;
 }
