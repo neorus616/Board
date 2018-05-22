a33870877579655a87692a624186cdcb40e8d979
neorus616 <33427780+neorus616@users.noreply.github.com>
2018-05-22 18:38:39 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,9 +7,9 @@ using namespace std;
 class Player{
     public:
         Player();
-        virtual const string name() const;
+        virtual const string name() const = 0;
         char myChar;
-        virtual const Coordinate play(const Board&);
-        virtual const char getChar() const;
+        virtual const Coordinate play(const Board&) = 0;
+        const char getChar() const;
         void setChar(char);
 };
